37bce210fc1b191a917fc20548db824bb23d3f8c
mster3313 <mahendra.d.b313@eng.pdn.ac.lk>
2020-05-04 00:59:47 +0530

    added morgan dependency and static file retrieving

--- node-express/index.js
+++ node-express/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server_app = express();
 
+server_app.use(morgan('dev'));
+
+server_app.use(express.static(__dirname + '/public'));
+
 server_app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><head><title>Welcome</title></head><body><h1>TO MY WORLD B!</h1></body></html>');
